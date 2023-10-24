d9cc0e9b638a0dddb97a2943ffbbe922d5319838
Anderson Domingos <anderson@mica12.com.br>
2023-10-24 10:08:41 -0300

    arquivo inicial

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem-vindo. Esse e o meu primeiro aplicativo java!");
     }
 }
